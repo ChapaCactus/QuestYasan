d33f71c00470b0ec5210a0d477699114faf4cae5
Chapa.C(Takuya) <chapatown@gmail.com>
2019-01-04 05:27:26 +0900

    DeckModel追加

---
+++ Assets/Scripts/DeckModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Google2u;
+
+namespace CCG
+{
+    /// <summary>
+    /// 山札 Model
+    /// </summary>
+    public class DeckModel
+    {
+        private const int MaxCardSize = 40;
+
+        public List<CardModel> Cards { get; private set; } = new List<CardModel>();
+
+        public bool IsEmpty => Cards.Any();
+        public bool IsFull => Cards.Count.Equals(MaxCardSize);
+
+        /// <summary>
+        /// カードを引く
+        /// </summary>
+        public CardModel Draw()
+        {
+            var card = Cards.FirstOrDefault();
+            Cards.Remove(card);
+
+            return card;
+        }
+    }
+}
