22b6b36f4077cdec02b636986adb9d74a7deef16
Chapa.C(Takuya) <chapatown@gmail.com>
2019-01-04 16:47:07 +0900

    (cs) HandModel追加

---
+++ Assets/Scripts/HandModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace CCG
+{
+    public class HandModel
+    {
+        private const int DefaultHandSize = 4;
+
+        public List<CardModel> Cards { get; private set; }
+
+        /// <summary>
+        /// 初期手札枚数までカードを引く
+        /// </summary>
+        public void DrawForDefaultSize()
+        {
+            if(Cards.Count >= DefaultHandSize)
+            {
+                return;
+            }
+        }
+    }
+}
--- Assets/Scripts/UserModel.cs
+++ Assets/Scripts/UserModel.cs
@@ -10,10 +10,23 @@ namespace CCG
         public DeckModel Deck { get; private set; }
 
         /// <summary>
+        /// 手札
+        /// </summary>
+        public HandModel Hand { get; private set; }
+
+        /// <summary>
         /// コンストラクタ
         /// </summary>
         public UserModel()
         {
+        }
+
+        /// <summary>
+        /// このユーザーターン開始時
+        /// </summary>
+        public void OnStartUserTurn()
+        {
+
         }
     }
 }
