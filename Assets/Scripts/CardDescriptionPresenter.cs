1d974bc8a3ef5edcf12dab9a686ee107f73dd20c
Chapa.C(Takuya) <chapatown@gmail.com>
2019-01-05 04:20:28 +0900

    カード説明文表示の準備

---
+++ Assets/Scripts/CardDescriptionPresenter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCG
+{
+    public class CardDescriptionPresenter : MonoBehaviour
+    {
+        private const string PrefabPath = "Prefabs/CardDescription";
+
+        public static CardDescriptionPresenter Create(Transform parent)
+        {
+            var prefab = Resources.Load<CardDescriptionPresenter>(PrefabPath);
+            return Instantiate(prefab, parent);
+        }
+    }
+}
