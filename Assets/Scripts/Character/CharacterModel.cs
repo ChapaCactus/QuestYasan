0ff4ba94e4c432dbaa9ee0a582521a3399c0124b
Chapa.C(Takuya) <chapatown@gmail.com>
2019-01-06 19:08:51 +0900

    Characterクラス追加

---
+++ Assets/Scripts/Character/CharacterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCG
+{
+    public class CharacterModel
+    {
+        public int CurrentFloorIndex { get; private set; } = 0;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public CharacterModel()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Character/CharacterPresenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCG
+{
+    [RequireComponent(typeof(CharacterView))]
+    public class CharacterPresenter : MonoBehaviour
+    {
+        private CharacterModel _model;
+        private CharacterView _view;
+    }
+}
